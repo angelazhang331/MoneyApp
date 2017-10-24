4f63662389aa6c9672b28300d2062191c57c8215
Angela <angelazhang331@gmail.com>
2017-10-24 13:40:57 -0700

    Messing with colors

--- app/src/main/java/com/example/angela/moneyapp/MainActivity.java
+++ app/src/main/java/com/example/angela/moneyapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.angela.moneyapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private ConstraintLayout background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test
+
+        wiringWidgets();
+    }
+
+    private void wiringWidgets() {
+        //background = (ConstraintLayout) findViewById(R.id.layout_main_background);
+        //background.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
     }
 }
