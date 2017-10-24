91320718bf0a9b18587c06a0b5ee80266dbb1853
Angela <angelazhang331@gmail.com>
2017-10-24 12:33:04 -0700

    Revert "Added Person class"
    This reverts commit e353be2791edd24b5490722cef94640878d5fe54.

--- app/src/main/java/com/example/angela/moneyapp/Person.java
+++ app/src/main/java/com/example/angela/moneyapp/Person.java
@@ -7,39 +7,9 @@ package com.example.angela.moneyapp;
 public class Person {
 
     //instance variables
-    private String name;
 
     //constructors
-    public Person(String name, String description, String date, int amount) {
-        this.name = name;
-//        this.description = description;
-//        this.date = date;
-//        this.amount = amount;
-    }
-
-    public Person(String name) {
-        this.name = name;
-    }
 
     //methods
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void addOwe(String date, String description, int amount) {
-        //Owe initial = new Owe( , , );
-    }
-
-    public void addOwe(String description, int amount) {
-        //Owe initial = new Owe( , );
-    }
-
-    public void addOwe(int amount) {
-        //Owe initial = new Owe();
-    }
 
 }
