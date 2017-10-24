e26875775ffc91183e0ca0c39c777bf2a20dd3f7
Angela <angelazhang331@gmail.com>
2017-10-24 12:35:59 -0700

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 997777efe5f5b011a507b23e5d61f05874387ad5, reversing
    changes made to 3dcdfe8babca09bdb46f421c8a85de679f356ef9.

--- app/src/main/java/com/example/angela/moneyapp/Person.java
+++ app/src/main/java/com/example/angela/moneyapp/Person.java
@@ -7,10 +7,40 @@ package com.example.angela.moneyapp;
 public class Person {
 
     //instance variables
+    private String name;
 
     //constructors
+    public Person(String name, String description, String date, int amount) {
+        this.name = name;
+//        this.description = description;
+//        this.date = date;
+//        this.amount = amount;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
 
     //methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addOwe(String date, String description, int amount) {
+        //Owe initial = new Owe( , , );
+    }
+
+    public void addOwe(String description, int amount) {
+        //Owe initial = new Owe( , );
+    }
+
+    public void addOwe(int amount) {
+        //Owe initial = new Owe();
+    }
 
     //trying to fix something?
 
