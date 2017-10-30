a9adbe8de3dd9f83caf416cd6ea92cff8cb0089b
BuildTools <unconfigured@null.spigotmc.org>
2017-10-30 14:31:04 -0700

    Merge branch 'master' of C:\Users\Skyler\AndroidStudioProjects\MoneyApp with conflicts.

--- app/src/main/java/com/example/angela/moneyapp/MainActivity.java
+++ app/src/main/java/com/example/angela/moneyapp/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.angela.moneyapp;
 
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,13 +16,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     //private ConstraintLayout background;
-    private ArrayList<Person> peopleList;
+    private ArrayList<Person> people;
     private ArrayAdapter<Person> peopleAdapter;
     private ListView peopleListView;
     private Button addPersonButton;
-    public static final String EXTRA_KEY = "key";
+    private AlertDialog.Builder addPersonBuilder;
+    private View addPersonView;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -31,22 +30,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         wiringWidgets();
         setOnClickListeners();
-        adaptingArrays();
-
-    }
-
-    private void adaptingArrays() {
-        peopleAdapter = new ArrayAdapter<Person>(this, R.layout.list_item_single_person, peopleList);
-        peopleListView.setAdapter(peopleAdapter);
-        peopleListView.setOnItemClickListener(new ListView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
-                Intent send = new Intent(MainActivity.this, PersonActivity.class);
-                send.putExtra(EXTRA_KEY, peopleList.get(pos));
-                startActivity(send);
-            }
-        });
-
     }
 
     private void setOnClickListeners() {
@@ -60,33 +43,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         addPersonButton = (Button) findViewById(R.id.button_add_person);
     }
 
+    private void addPerson() {
+        AlertDialog.Builder addPersonBuilder = new AlertDialog.Builder(MainActivity.this);
+        View addPersonView = getLayoutInflater().inflate(R.layout.pop_up_add_person, null);
+        final AlertDialog dialog = addPersonBuilder.create();
+
+        //Wiring the dialog widgets
+        final EditText etName = (EditText) addPersonView.findViewById(R.id.editText_pop_up_add_name);
+        final EditText etAmountOwed = (EditText) addPersonView.findViewById(R.id.editText_pop_up_add_person_amount_owed);
+        final EditText etDate = (EditText) addPersonView.findViewById(R.id.editText_pop_up_add_person_date);
+        final EditText etDescription = (EditText) addPersonView.findViewById(R.id.editText_pop_up_add_person_description);
+        Button submit = (Button) addPersonView.findViewById(R.id.button_pop_up_add_person_submit);
+        Button cancel = (Button) addPersonView.findViewById(R.id.button_pop_up_add_person_cancel);
+
+        /**
+         * When submitted, we'll convert the data of our widget variables to be applicable for the 'Person' class
+         * The dialog will not accept an empty name field, all other fields are optional
+         */
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!etName.getText().toString().isEmpty()){ //if the name field is NOT empty
+                    //First, convert the EditText to String variables
+                    String sName = etName.getText().toString();
+                    String sAmountOwed = etAmountOwed.getText().toString();
+                    String sDate = etDate.getText().toString();
+                    String sDescription = etDescription.getText().toString();
+
+                    //Second, convert our amount owed to a double
+                    double dAmountOwed = Double.parseDouble(sAmountOwed);
+
+                    //Third, we'll create a new Person and add them to the arrayList of people
+                    people.add(new Person(sName, sDescription, sDate, dAmountOwed));
+                    dialog.dismiss();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Please fill in the name field", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.setView(addPersonView);
+        dialog.show();
+    }
+
     @Override
     public void onClick(View view) {
         switch(view.getId()){
-            //TODO change this to the first slot on the listview?
             case R.id.button_add_person:
-                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
-                View mView = getLayoutInflater().inflate(R.layout.activity_pop_up_add_person, null);
-                final EditText mPersonName = (EditText) mView.findViewById(R.id.editText_pop_up_add_name);
-                EditText mAmountOwed = (EditText) mView.findViewById(R.id.editText_pop_up_add_person_amount_owed);
-                EditText mDate = (EditText) mView.findViewById(R.id.editText_pop_up_add_person_date);
-                EditText mDescription = (EditText) mView.findViewById(R.id.editText_pop_up_add_person_description);
-                Button mSubmit = (Button) mView.findViewById(R.id.button_pop_up_add_person_submit);
-
-                mSubmit.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if(!mPersonName.getText().toString().isEmpty()){
-                            Toast.makeText(MainActivity.this, "If this shows it worked", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            Toast.makeText(MainActivity.this, "aiohufijuhaioj", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-                mBuilder.setView(mView);
-                AlertDialog dialog = mBuilder.create();
-                dialog.show();
+                addPerson();
                 break;
             default:
                 break;
