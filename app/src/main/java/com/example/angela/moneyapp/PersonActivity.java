b27623cceda2203d4f48b08a19dcc2062e6c5d50
Angela <angelazhang331@gmail.com>
2017-11-01 13:57:58 -0700

    try start sonetgubg vyt inot

--- app/src/main/java/com/example/angela/moneyapp/PersonActivity.java
+++ app/src/main/java/com/example/angela/moneyapp/PersonActivity.java
@@ -2,16 +2,18 @@ package com.example.angela.moneyapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class PersonActivity extends AppCompatActivity {
+public class PersonActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String personName;
     private TextView nameTextView;
@@ -19,6 +21,7 @@ public class PersonActivity extends AppCompatActivity {
     private ArrayList<Owe> oweList;
     private ArrayAdapter<Owe> oweAdapter;
     private Person currentPerson;
+    private Button addAmountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,7 @@ public class PersonActivity extends AppCompatActivity {
         setContentView(R.layout.activity_person);
 
         wiringWidgets();
+        setOnClickListeners();
 
 //        adapter = new ArrayAdapter<>(this, R.layout.list_item_color, colorList);
 //        colorListView.setAdapter(adapter);
@@ -42,10 +46,15 @@ public class PersonActivity extends AppCompatActivity {
 
         Intent get = getIntent();
         personName = get.getStringExtra(MainActivity.EXTRA_KEY);
+        nameTextView.setText(personName);
         currentPerson = get.getParcelableExtra(MainActivity.EXTRA_KEY);
         oweList = currentPerson.getOweList();
         adaptArray();
 
+    }
+
+    private void setOnClickListeners() {
+        addAmountButton.setOnClickListener(this);
     }
 
     private void adaptArray() {
@@ -61,5 +70,79 @@ public class PersonActivity extends AppCompatActivity {
     private void wiringWidgets() {
         nameTextView = (TextView) findViewById(R.id.textView_name);
         oweListView = (ListView) findViewById(R.id.listView_owes);
+        addAmountButton = (Button) findViewById(R.id.button_new_amount);
+    }
+
+    @Override
+    public void onClick(View view) {
+        addAmount();
+    }
+
+    private void addAmount() {
+        AlertDialog.Builder addAmountBuilder = new AlertDialog.Builder(PersonActivity.this);
+        View addAmountView = getLayoutInflater().inflate(R.layout.activity_pop_up_add_amount, null);
+        final AlertDialog dialog = addAmountBuilder.create();
+
+        //Wiring the dialog widgets
+//        final EditText etName = (EditText) addPersonView.findViewById(R.id.editText_pop_up_add_name);
+//        final EditText etAmountOwed = (EditText) addPersonView.findViewById(R.id.editText_pop_up_add_person_amount_owed);
+//        final EditText etDate = (EditText) addPersonView.findViewById(R.id.editText_pop_up_add_person_date);
+//        final EditText etDescription = (EditText) addPersonView.findViewById(R.id.editText_pop_up_add_person_description);
+//        Button submit = (Button) addPersonView.findViewById(R.id.button_pop_up_add_person_submit);
+//        Button cancel = (Button) addPersonView.findViewById(R.id.button_pop_up_add_person_cancel);
+//
+//        /**
+//         * When submitted, we'll convert the data of our widget variables to be applicable for the 'Person' class
+//         * The dialog will not accept an empty name field, all other fields are optional
+//         */
+//        submit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(!etName.getText().toString().isEmpty()){ //if the name field is NOT empty
+//
+//                    String sName = etName.getText().toString();
+//                    String sAmountOwed = etAmountOwed.getText().toString();
+//                    String sDate = etDate.getText().toString();
+//                    String sDescription = etDescription.getText().toString();
+//                    int iAmountOwed = 0;
+//
+//                    if (!sAmountOwed.equals("")) {
+//                        //Second, convert our amount owed to a double
+//                        iAmountOwed = Integer.parseInt(sAmountOwed);
+//                    }
+//                    //First, convert the EditText to String variables
+//                    if (!sDescription.equals("")) {
+//                        if (sDate.equals("")) {
+//                            Toast.makeText(MainActivity.this, "Please enter a date", Toast.LENGTH_SHORT).show();
+//                        }
+//                        else {
+//                            peopleList.add(new Person(sName, sDescription, sDate, iAmountOwed));
+//                        }
+//                    }
+//                    else {
+//                        if(!sDate.equals("")){
+//                            peopleList.add(new Person(sName));
+//                        }
+//                        else {
+//                            peopleList.add(new Person(sName, sDate, iAmountOwed));
+//                        }
+//                    }
+//                    dialog.dismiss();
+//                }
+//                else {
+//                    Toast.makeText(MainActivity.this, "Please fill in the name field", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//
+//        cancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dialog.dismiss();
+//            }
+//        });
+//
+//        dialog.setView(addPersonView);
+//        dialog.show();
     }
 }
