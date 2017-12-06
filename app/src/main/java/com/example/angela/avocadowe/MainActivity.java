eadc3e0a439dae474df73839bcb94ffc3b2ded33
Angela <angelazhang331@gmail.com>
2017-12-06 13:56:56 -0800

    maybe borken

--- app/src/main/java/com/example/angela/avocadowe/MainActivity.java
+++ app/src/main/java/com/example/angela/avocadowe/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.angela.moneyapp;
+package com.example.angela.avocadowe;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -16,6 +16,7 @@ import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
+import com.example.angela.moneyapp.R;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -173,12 +174,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     String sDescription = etDescription.getText().toString();
                     int iAmountOwed = 0;
 
-                    if (!sAmountOwed.equals("")) {
+                    if (!sAmountOwed.equals("")) { //if there is an amount, then require a date
                         //Second, convert our amount owed to an integer
                         iAmountOwed = Integer.parseInt(sAmountOwed);
-                    }
-                    
-                    if (!sDescription.equals("")) {
                         if (sDate.equals("")) {
                             Toast.makeText(MainActivity.this, "Please enter a date", Toast.LENGTH_SHORT).show();
                         }
@@ -188,25 +186,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             json =  gson.toJson(peopleList);
                             preferenceEditor.putString("MyArray", json);
                             preferenceEditor.apply();
-                        }
-                    }
-                    else {
-                        if(!sDate.equals("")){
-                            peopleList.add(new Person(sName));
-                            json =  gson.toJson(peopleList);
-                            preferenceEditor.putString("MyArray", json);
-                            preferenceEditor.apply();
-                        }
-                        else {
-                            peopleList.add(new Person(sName, sDate, iAmountOwed));
 
-                            json =  gson.toJson(peopleList);
-                            preferenceEditor.putString("MyArray", json);
-                            preferenceEditor.apply();
+                            dialog.dismiss();
+                            sortByName();
                         }
                     }
-                    dialog.dismiss();
-                    sortByName();
+                    else { //if there is no amount
+                        peopleList.add(new Person(sName));
+
+                        json = gson.toJson(peopleList);
+                        preferenceEditor.putString("MyArray", json);
+                        preferenceEditor.apply();
+
+                        dialog.dismiss();
+                        sortByName();
+                    }
                 }
                 else {
                     Toast.makeText(MainActivity.this, "Please fill in the name field", Toast.LENGTH_SHORT).show();
@@ -258,16 +252,19 @@ Ideas
  */
 
 /*
-TODO have the app check if they are inputting an amount to see if date is required
-if not, find a replacement or temporary value for the date
-or
-completely remove the option to add an amount while creating a new person
-
-could have fields that ask for email and phone number or something like that instead of date and amount
-
-TODO: in the amount paid, include something showing how much is still owed; add labels by the numbers; have the toast say paid to which date
 
 TODO: have the textview for each listview item change color to show whether it was paid or not. Update the array for whether or not the thing was paid and the amount paid
 
 TODO transfer the extra pay to the next owe?
+
+ Change person name font
+ change button colors for accent
+
+ change button styles
+ change font size for view owe
+
+ format textview list item size
+ pictures?
+
+
  */
--- app/src/main/java/com/example/angela/avocadowe/Person.java
+++ app/src/main/java/com/example/angela/avocadowe/Person.java
@@ -1,4 +1,4 @@
-package com.example.angela.moneyapp;
+package com.example.angela.avocadowe;
 
 import android.os.Parcel;
 import android.os.Parcelable;
